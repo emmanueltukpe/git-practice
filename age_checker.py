06ee9a59fb02b673bcc1e0d22aff2cf61e3dad5a
emmanueltukpe <tukpeemmanuel@gmail.com>
2020-08-13 19:04:31 +0100

    formatted the document

--- age_checker.py
+++ age_checker.py
@@ -1,19 +1,19 @@
 def age_check():
-    age_str = input( 'How old are you?: ')
+    age_str = input('How old are you?: ')
     try:
         y = int(age_str)
     except:
-        print ("provide your age in numbers")
+        print("provide your age in numbers")
         return age_check()
-
     else:
         return y
 
+
 run_var = "Y"
 while run_var == "Y":
-    print ('Welcome')
-    print ("Please provide your name and age")
-    print ("Let's get started")
+    print('Welcome')
+    print("Please provide your name and age")
+    print("Let's get started")
 
     first_name = input("Provide your first name")
     last_name = input("Provide your last name")
@@ -21,5 +21,6 @@ while run_var == "Y":
     year = 2020
     year_of_birth = 2020 - age
 
-    print (f"Hello {first_name}{last_name}", f"you were born in the year {year_of_birth}")
+    print(f"Hello {first_name}{last_name}",
+          f"you were born in the year {year_of_birth}")
     run_varun_var = input('Do you want to try again')
